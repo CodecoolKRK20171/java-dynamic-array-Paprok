acb4ce821434cce26732a7ad734d7adc8990c962
Paprok <rafal.becla@gmail.com>
2019-01-28 18:40:49 +0100

    passed addTest

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -2,5 +2,67 @@ package com.codecool.dynamicArrayDojo;
 
 // put your code here!
 public class DynamicIntArray {
+    private int[] array;
+    private int index;
+    private int size;
 
+    public DynamicIntArray() {
+        this.size = 16;
+        this.array = new int[size];
+        this.index = 0;
+    }
+
+    public DynamicIntArray(int size) {
+        this.size = 16;
+        this.array = new int[size];
+        this.index = 0;
+    }
+
+    public void add(int item) {
+        if (index + 1 < size) {
+            append(item);
+        } else {
+            doubleSize();
+            append(item);
+        }
+    }
+
+    private void append(int item) {
+        this.array[index] = item;
+        this.index++;
+    }
+
+    private void doubleSize() {
+        int[] temp = this.array.clone();
+        this.size *= 2;
+        this.array = new int[size];
+        save(temp);
+    }
+
+    private void save(int[] temp) {
+        for (index = 0; index < temp.length; index++) {
+            this.array[index] = temp[index];
+        }
+    }
+
+    public void insert(int insertId, int item) {
+
+    }
+
+    public void remove(int index) throws ArrayIndexOutOfBoundsException {
+
+    }
+
+    public String toString() {
+        StringBuilder toReturn = new StringBuilder(" ");
+        int end = index - 1;
+        int i;
+        for (i = 0; i < end; i++) {
+            toReturn.append(array[i]);
+            toReturn.append(' ');
+        }
+        toReturn.append(array[i]);
+
+        return toReturn.toString();
+    }
 }
